f8c9e2101173c38a198c2097a32565688ec78892
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-05-09 10:27:19 +0300

    feat: добавить обработку пользовательских нажатий

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,21 @@
 const panel = document.querySelector('.panel');
 const startButton = panel.querySelector('.panel__button-start');
+const levelPanel = panel.querySelector('.panel__level');
 const sectors = panel.querySelectorAll('.panel__sector');
+let level = 0;
+let gameOrder = [];
+let playerOrder = [];
 
-function startGame() {
-  hideStartButton();
-  const consiquence = [];
-  increaseConsiquence(consiquence);
-  showConsiquence(consiquence);
+function startNewLevel() {
+  playerOrder = [];
+  increaseLevel();
+  increaseGameOrder(gameOrder);
+  showGameOrder(gameOrder);
+}
+
+function increaseLevel() {
+  level++;
+  levelPanel.textContent = level;
 }
 
 function hideStartButton() {
@@ -19,24 +28,52 @@ function showStartButton() {
   startButton.removeAttribute('disabled', '');
 }
 
-function increaseConsiquence(consiquence) {
+function increaseGameOrder() {
   const randomSector = Math.floor(Math.random() * 4);
-  consiquence.push(randomSector);
+  gameOrder.push(randomSector);
 }
 
-function showConsiquence(consiquence) {
-  for (let i = 0; i < consiquence.length; i++) {
+function showGameOrder() {
+  for (let i = 0; i < gameOrder.length; i++) {
     setTimeout(function timer() {
-      blinkSector(consiquence[i]);
+      blinkSector(gameOrder[i]);
     }, i * 1000);
   }
 }
 
 function blinkSector(sectorNumber) {
-  setTimeout(function() {
     sectors[sectorNumber].classList.add('panel__sector_blink');
     setTimeout(() => sectors[sectorNumber].classList.remove('panel__sector_blink'), 500)
-  }, 1000)
 }
 
-startButton.addEventListener('click', startGame);
+function checkPlayerOrder() {
+  let check = playerOrder[playerOrder.length - 1] === gameOrder[playerOrder.length - 1];
+  console.log(`gameOrder: ${gameOrder} - playerOrder: ${playerOrder}`);
+  if (!check) {
+    gameover()
+  }
+  if (check && playerOrder.length === gameOrder.length) {
+    setTimeout(() => startNewLevel(), 2000);
+  }
+}
+
+function gameover() {
+  levelPanel.textContent = 'fail';
+}
+
+function addToOrder(sector) {
+  const sectorNumber = +sector.id[7];
+  playerOrder.push(sectorNumber);
+}
+
+sectors.forEach(sector => sector.addEventListener('click', (evt) => {
+  blinkSector(evt.target.id[7]);
+  addToOrder(evt.target);
+  checkPlayerOrder();
+}));
+
+startButton.addEventListener('click', () => {
+  hideStartButton();
+  startNewLevel();
+})
+
